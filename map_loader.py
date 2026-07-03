43cd5446f0f74b852415cfe4a925c9ce695ab670
ItsAndreww <andryi.novokhatskyi@gmail.com>
2026-07-03 17:37:49 +0300

    Switch to Nuitka to avoid AV false positives and fix resource path

--- map_loader.py
+++ map_loader.py
@@ -96,8 +96,9 @@ def resource_path(relative_path):
     try:
         # PyInstaller створює тимчасову папку _MEIPASS
         base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
+    except AttributeError:
+        # Nuitka та звичайний запуск
+        base_path = os.path.dirname(os.path.abspath(__file__))
 
     return os.path.join(base_path, relative_path)
 
